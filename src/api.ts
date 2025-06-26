3be01e985e59e32dbcb7af1d077a70d0464d2742
Angry Toenail <216561774+angrytoenail@users.noreply.github.com>
2025-06-26 06:13:31 +0100

    Add functions for querying SQL via GraphQL

--- src/api.ts
+++ src/api.ts
@@ -10,3 +10,24 @@ export default api;
 const { gql, useMutation } = Apollo;
 const defaultOptions = {};
 
+export const QuerySQLDocument = gql`
+  mutation QuerySQL($sql: String!, $args: [Any]) {
+    querySQL(sql: $sql, args: $args) {
+      columns
+      rows
+    }
+  }
+`;
+
+export function useQuerySQLMutation(
+  baseOptions?: MutationHookOptions<
+    QuerySQLMutation,
+    QuerySQLMutationVariables
+  >,
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return useMutation<QuerySQLMutation, QuerySQLMutationVariables>(
+    QuerySQLDocument,
+    options,
+  );
+}
